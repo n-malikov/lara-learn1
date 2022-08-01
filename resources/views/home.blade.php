9ab8b8447019cbcf6aaf1f0c54671ff1b91f97e9
nail-php <malikov.kzn@gmail.com>
2022-08-01 13:24:47 +0300

    lesson 1: php artisan vendor:publish

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,25 @@
                         </div>
                     @endif
 
-                    Your site
+                    <p>
+                        без пакетов для разработчика<br>
+                        <code>composer install --no-dev</code>
+                    </p>
+
+                    <h4>IDE Helper</h4>
+                    <p>
+                        добавляем Laravel IDE Helper<br>
+                        <code>composer require --dev barryvdh/laravel-ide-helper</code>
+                    </p>
+                    <p>
+                        скопируем файл настроек из vendor в config нашего проекта<br>
+                        <code>php artisan vendor:publish</code><br>
+                        и далее выбираем нужный модуль из списка<br>
+                        <code>php artisan vendor:publish --provider="Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider" --tag=config</code><br>
+                        в конце не забываем сгенерировать по новой:<br>
+                        <code>php artisan ide-helper:generate</code>
+                    </p>
+
                 </div>
             </div>
         </div>
