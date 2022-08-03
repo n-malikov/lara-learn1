b341cb2219a59372975423e279dbc660af9003f6
nail-php <malikov.kzn@gmail.com>
2022-08-03 13:11:54 +0300

    lesson 1: Node(yarn, npm), Webpack

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,9 +14,20 @@
                         </div>
                     @endif
 
+                    <h2><code>laralearn</code> - введи в поиске по проекту, это комментарии из уроков</h2>
+
                     <p>
                         без пакетов для разработчика<br>
                         <code>composer install --no-dev</code>
+                    </p>
+
+                    <h4>Node</h4>
+                    <p>
+                        <code>nvm install 16.14.2</code><br>
+                        <code>nvm use 16.14.2</code><br>
+                        <code>yarn</code><br>
+                        <code>npm install</code><br>
+                        <code>npm run dev</code> или <code>npm run prod</code> - все варианты в /package.json
                     </p>
 
                     <h4>IDE Helper</h4>
@@ -31,6 +42,17 @@
                         <code>php artisan vendor:publish --provider="Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider" --tag=config</code><br>
                         в конце не забываем сгенерировать по новой:<br>
                         <code>php artisan ide-helper:generate</code>
+                    </p>
+
+                    <h4>Webpack</h4>
+                    <p>
+                        <code>/webpack.mix.js</code> - упрощенный конфиг для сборщика модулей webpack<br>
+                        поменять путь, куда webpack будет складывать свои файлы можно в этом же файле
+                    </p>
+                    <p>
+                        Борьба с кэшем браузеров:<br>
+                        в /webpack.mix.js добавить <code>.version()</code>, теперь все файлы будут генериться с get параметром<br>
+                        в фалах темы сменить все <code>asset('build/js/app.js')</code> на <code>mix('js/app.js', 'build')</code> (второй параметр это папка где лежит)
                     </p>
 
                 </div>
