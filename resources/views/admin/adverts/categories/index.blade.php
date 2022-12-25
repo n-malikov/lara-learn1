71d83037892afc601bca26f3fa3924895b65d9fa
n-malikov <nail1000@gmail.com>
2022-12-25 23:48:26 +0300

    сортировка категорий [4.2]

--- resources/views/admin/adverts/categories/index.blade.php
+++ resources/views/admin/adverts/categories/index.blade.php
@@ -5,11 +5,12 @@
 
     <p><a href="{{ route('admin.adverts.categories.create') }}" class="btn btn-success">Add Category</a></p>
 
-    <table>
+    <table class="table table-bordered table-striped">
         <thead>
         <tr>
             <th>Name</th>
             <th>Slug</th>
+            <th></th>
         </tr>
         </thead>
         <tbody>
@@ -22,6 +23,24 @@
                     <a href="{{ route('admin.adverts.categories.show', $category) }}">{{ $category->name }}</a>
                 </td>
                 <td>{{ $category->slug }}</td>
+                <td class="d-flex flex-row">
+                    <form method="POST" action="{{ route('admin.adverts.categories.first', $category) }}" class="mr-1">
+                        @csrf
+                        <button class="btn btn-sm btn-outline-primary">First</button>
+                    </form>
+                    <form method="POST" action="{{ route('admin.adverts.categories.up', $category) }}" class="mr-1">
+                        @csrf
+                        <button class="btn btn-sm btn-outline-primary">Up</button>
+                    </form>
+                    <form method="POST" action="{{ route('admin.adverts.categories.down', $category) }}" class="mr-1">
+                        @csrf
+                        <button class="btn btn-sm btn-outline-primary">Down</button>
+                    </form>
+                    <form method="POST" action="{{ route('admin.adverts.categories.last', $category) }}" class="mr-1">
+                        @csrf
+                        <button class="btn btn-sm btn-outline-primary">Last</button>
+                    </form>
+                </td>
             </tr>
         @endforeach
 
